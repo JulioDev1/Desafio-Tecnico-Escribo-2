abb213565e6399a5a8020b888a3a04033d178a32
Julio Cesar <juliocesargualberto617@gmail.com>
2023-11-22 16:38:13 -0300

    test: implement all error case

--- src/adpaters/RegisterController.spec.ts
+++ src/adpaters/RegisterController.spec.ts
@@ -5,12 +5,55 @@ import express from "express"
 
 const app = express()
 describe("register", () => {
-  test("should be register user", async () => {
+  test("should be return erro because password is null", async () => {
     const response = await request(app)
       .post("/create")
       .send({
         name: "anyName",
         email: "anymail17@mail.com.br",
+        telephone: [{ ddd: "11", phone: "55555555" }],
+      })
+
+    expect(response.statusCode).toBe(404)
+  })
+  test("should be return erro because email is null", async () => {
+    const response = await request(app)
+      .post("/create")
+      .send({
+        name: "anyName",
+        password: "anypassword",
+        telephone: [{ ddd: "11", phone: "55555555" }],
+      })
+
+    expect(response.statusCode).toBe(404)
+  })
+  test("should be return erro because name is null", async () => {
+    const response = await request(app)
+      .post("/create")
+      .send({
+        email: "anymail17@mail.com.br",
+        password: "anypassword",
+        telephone: [{ ddd: "11", phone: "55555555" }],
+      })
+
+    expect(response.statusCode).toBe(404)
+  })
+  test("should be return error because telephone is null", async () => {
+    const response = await request(app).post("/create").send({
+      name: "anyName",
+      email: "anymail17@mail.com.br",
+      password: "anypassword",
+    })
+
+    expect(response.statusCode).toBe(404)
+  })
+  test("should be return error because email already register", async () => {
+    const response = await request(app)
+      .post("/create")
+      .send({
+        name: "anyName",
+        email: "anymail@mail.com.br",
+        password: "anypassword",
         telephone: [{ ddd: "11", phone: "55555555" }],
       })
 
